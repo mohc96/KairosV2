70c288111c7393cadc3dfb759cd52b8dba7ae217
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-09-20 18:15:14 -0700

    fix: Create Project Version 2 for socket streaming

--- client/src/components/StudentDashboard.jsx
+++ client/src/components/StudentDashboard.jsx
@@ -5,20 +5,18 @@ import AboutMe from './AboutMe';
 import ExpertSearchComponent from './WhoCanHelp';
 import StudentProjects from './StudentProjects/StudentProjects';
 import SidebarMorningPulse from './MorningPulse';
-import CreateProjectAI from './CreateProject/CreateProjectAI';
-import StreamingProjectCreator from './CreateProject/StreamingProjectCreator';
+import CreateProject from './CreateProject/CreateProject';
 
 export default function StudentDashboard({ email }) {
   return (
     <div>
       <IntroductionContainer />
       <SidebarMorningPulse />
-      <CreateProjectAI />
+      <CreateProject/>
       <StudentProjects />
       <SidebarAdvice />
       <ExpertSearchComponent />
       <AboutMe />
-      <StreamingProjectCreator/>
       <SidebarBreakTimer />
     </div>
   );
