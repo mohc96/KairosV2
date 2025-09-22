95224ca61b55433bf1b524e9ab7e37babcd60ee5
Venkata Sai Harsha Vardhan Mirthinti <86153806+mhv2408@users.noreply.github.com>
2025-09-22 15:32:27 -0700

    Update StudentDashboard.jsx: removed extra commits

--- client/src/components/StudentDashboard.jsx
+++ client/src/components/StudentDashboard.jsx
@@ -3,15 +3,10 @@ import SidebarAdvice from './Advice';
 import SidebarBreakTimer from './Break';
 import AboutMe from './AboutMe';
 import ExpertSearchComponent from './WhoCanHelp';
-<<<<<<< HEAD
-import StudentProjects from './StudentProjects';
 import GuideMe from './GuideMe';
-
-=======
 import StudentProjects from './StudentProjects/StudentProjects';
 import SidebarMorningPulse from './MorningPulse';
 import CreateProject from './CreateProject/CreateProject';
->>>>>>> 70c288111c7393cadc3dfb759cd52b8dba7ae217
 
 export default function StudentDashboard({ email }) {
   return (
