b02c4eac89bacb1934214a3ca251654869f19fa6
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-09-22 17:12:00 -0700

    Guideme: fixed import errors

--- client/src/components/GuideMe.jsx
+++ client/src/components/GuideMe.jsx
@@ -1,4 +1,4 @@
-
+import React, { useState, useEffect } from 'react';
 import {
     Clock,
     Download,
@@ -6,11 +6,8 @@ import {
     ThumbsUp,
     ThumbsDown,
     Send,
-    User,
-    GraduationCap,
     ChevronDown,
     ChevronUp,
-    MessageCircle,
     Bot,
     BotMessageSquare,
     Bug
